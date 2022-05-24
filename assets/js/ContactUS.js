d6970a38fa895c07d5d4bc18deb636b065a48c72
Carlos <carlos.misterio@live.com.mx>
2022-05-24 01:16:32 -0500

    Añadí local storage y corregí errores de ortografía en diversas partes de la página de contáctenos

---
+++ assets/js/ContactUS.js
@@ -0,0 +1,8 @@
+function guardarDatos() {
+    localStorage.nombre = document.getElementById("nombre").value;
+    localStorage.apellido = document.getElementById("apellido").value; 
+    localStorage.telefono = document.getElementById("telefono").value; 
+    localStorage.correo = document.getElementById("correo").value; 
+localStorage.message = document.getElementById("message").value; 
+}
+
