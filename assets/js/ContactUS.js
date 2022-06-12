1a941b5e089cf27a2208fd46ca8fe2330c56ae88
ricardo55 <pegassoric@hotmail.com>
2022-06-11 21:57:48 -0500

    Actualice rama con archivos ya completos de pagina principal

--- assets/js/ContactUS.js
+++
@@ -1,8 +0,0 @@
-function guardarDatos() {
-    localStorage.nombre = document.getElementById("nombre").value;
-    localStorage.apellido = document.getElementById("apellido").value; 
-    localStorage.telefono = document.getElementById("telefono").value; 
-    localStorage.correo = document.getElementById("correo").value; 
-localStorage.message = document.getElementById("message").value; 
-}
-
